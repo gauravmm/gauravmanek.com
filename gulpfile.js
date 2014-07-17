ba945fd0a876102cc1440a06b1eff71bd52bf9ea
Gaurav Manek <gaurav@gauravmanek.com>
2014-07-17 23:22:22 +0800

    Toolchain almost complete

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@ var gulp = require('gulp');
 
 var exec = require('child_process').exec;   // Jekyll
 var gulpif = require('gulp-if');            // Conditional processing of Jekyll HTML output
-var htmlmin = require('gulp-htmlmin');
+var htmlmin = require('gulp-htmlmin');      // Minimize HTML
 var uglify = require('gulp-uglify');        // JS Processing
 var less = require('gulp-less');            // Less
+var uncss = require('gulp-uncss');          // Remove unused CSS
+var minifyCSS = require('gulp-minify-css'); // Minify CSS
+var glob = require('glob');                 // Used to list all HTML files.
 var imagemin = require('gulp-imagemin');    // Image processing
 var rimraf = require('gulp-rimraf');        // Clean
 var gutil = require('gulp-util');           // Utitlites [noop(), ...]
@@ -13,9 +16,15 @@ var debug = require('gulp-debug');          // debug
 
 
 
+
+//
+//// Config
+//
+
+
 var paths = {
   source: 'source/',  // The design source
-  ignore: '!*/_*',    // Ignore all things that have underscores in them
+  ignore: '!**/_*{,/**}',    // Ignore all things that have underscores in them
   source_jekyll: '.', // The location of the _config.yml file
   dest_jekyll: '_site/', // The location of the _config.yml file
   glob_jekyll_minify_regex: /.*\.html/, // All the jekyll files to minify
@@ -31,20 +40,25 @@ var transforms = {
 };
 
 
+
 //
-// Clear
+//// Clear
 //
+
 gulp.task('clean', function(cb) {
   // You can use multiple globbing patterns as you would with `gulp.src`
   return gulp.src([paths.dest + "*", paths.dest_jekyll + "*"], { read: false })
     .pipe(rimraf());
 });
 
+
+
 //
-// Build
+//// Build
 //
 
-// Jekyll
+//// Jekyll
+
 gulp.task('build-jekyll-run', ['clean'], function(cb) {
   exec('jekyll build', function(err) { 
     if (err) return cb(err); //return error
@@ -63,17 +77,23 @@ gulp.task('build-jekyll', ['build-jekyll-html'], function() {
     .pipe(rimraf());
 });
 
+//// Static Files
+
 // Copy all static files from source/ to publish/
 gulp.task('build-copystatic', ['build-jekyll'], function() {
   return gulp.src([paths.source + "**", paths.ignore], { base: paths.source })
     .pipe(gulp.dest(paths.dest));
 });
 
+//// Transformations
+
 // Perform all transforms.lessc
-gulp.task('build-lessc', ['build-copystatic'], function() {
+gulp.task('build-lessc', ['build-copystatic', 'build-jekyll'], function() {
   if (!transforms.lessc) throw new Error("transforms.lessc not defined");
   return gulp.src(transforms.lessc.from, { cwd: paths.source })
     .pipe(less())
+    .pipe(uncss({ html: glob.sync(paths.dest + "**/*.html") }))
+    .pipe(minifyCSS({noRebase:true, keepSpecialComments:0}))
     .pipe(gulp.dest(paths.dest + transforms.lessc.to));
 });
 gulp.task('build-jsmin', ['build-copystatic'], function() {
@@ -82,17 +102,16 @@ gulp.task('build-jsmin', ['build-copystatic'], function() {
     .pipe(uglify())
     .pipe(gulp.dest(paths.dest + transforms.js.to));
 });
-gulp.task('build', ['clean', 'build-jekyll', 'build-copystatic', 'build-lessc', 'build-jsmin']);
+gulp.task('build', ['clean', 'build-jekyll', 'build-copystatic', 'build-lessc', 'build-jsmin'], function(){
 
-/*
-// Copy all static images
-gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
-    // Pass in options to the task
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img'));
 });
-*/
+
+
+
+//
+//// Watchers
+//
+
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
