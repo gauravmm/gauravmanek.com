06e3bb9d017bc78528e8aa1d561e5f6ce85d549d
Gaurav Manek <gaurav@gauravmanek.com>
2014-07-18 15:56:12 +0800

    Abstracted over image processing stream.

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ var rimraf = require('gulp-rimraf');        // Clean
 var gutil = require('gulp-util');           // Utitlites [noop(), ...]
 var debug = require('gulp-debug');          // debug
 var es = require('event-stream');           // For parallel processing of streams
+var lazypipe = require('lazypipe');			// For abstracting over streams
 
 
 
@@ -28,21 +29,21 @@ var es = require('event-stream');           // For parallel processing of stream
 
 
 var paths = {
-  source: 'source/',  // The design source
-  content: '_content/',
-    ignore: '!**/_*{,/**}',    // Ignore all things that have underscores in them
-  source_jekyll: '.', // The location of the _config.yml file
-  dest_jekyll: '_site/', // The location of the _config.yml file
-  glob_jekyll_minify_regex: /.*\.html/, // All the jekyll files to minify
-  dest:   'publish/'    // The destination of the compiled site
+	source: 'source/',  // The design source
+	content: '_content/',
+		ignore: '!**/_*{,/**}',    // Ignore all things that have underscores in them
+	source_jekyll: '.', // The location of the _config.yml file
+	dest_jekyll: '_site/', // The location of the _config.yml file
+	glob_jekyll_minify_regex: /.*\.html/, // All the jekyll files to minify
+	dest:   'publish/'    // The destination of the compiled site
 };
 
 // Transforms have relative paths.
 var transforms = {
-  lessc: {from: ["_less/main.less"], to: "css/"},
-  js: {from: ["_js/*"], to: "js/"},
-  cssmin: [{files: ["css/main.css"]}],
-  imagemin: {parent: '_posts/', folder_prefix: "_", new_name: "img/", styles: [{name: "thumb", max_width: 600, max_height: 400}, {name: "full", max_width: 2048, max_height: 2048}]}
+	lessc: {from: ["_less/main.less"], to: "css/"},
+	js: {from: ["_js/*"], to: "js/"},
+	cssmin: [{files: ["css/main.css"]}],
+	imagemin: {parent: '_posts/', folder_prefix: "_", new_name: "img/", styles: [{name: "thumb", max_width: 600, max_height: 400}, {name: "full", max_width: 2048, max_height: 2048}]}
 };
 
 
@@ -52,9 +53,9 @@ var transforms = {
 //
 
 gulp.task('clean', function(cb) {
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  return gulp.src([paths.dest + "*", paths.dest_jekyll + "*"], { read: false })
-    .pipe(rimraf());
+	// You can use multiple globbing patterns as you would with `gulp.src`
+	return gulp.src([paths.dest + "*", paths.dest_jekyll + "*"], { read: false })
+		.pipe(rimraf());
 });
 
 
@@ -67,23 +68,23 @@ gulp.task('clean', function(cb) {
 //// Jekyll
 
 gulp.task('build-jekyll-run', ['clean'], function(cb) {
-  exec('jekyll build', function(err) { 
-    if (err) return cb(err); //return error
-    cb(); // finished task
-  });
+	exec('jekyll build', function(err) { 
+		if (err) return cb(err); //return error
+		cb(); // finished task
+	});
 });
 
 gulp.task('build-jekyll-html', ['build-jekyll-run'], function() {
-  return gulp.src(paths.dest_jekyll + "**", { base: paths.dest_jekyll })
-    // Only process .html files:
-    .pipe(gulpif(paths.glob_jekyll_minify_regex, htmlmin({collapseWhitespace: true})))
-    .pipe(gulp.dest(paths.dest));
+	return gulp.src(paths.dest_jekyll + "**", { base: paths.dest_jekyll })
+		// Only process .html files:
+		.pipe(gulpif(paths.glob_jekyll_minify_regex, htmlmin({collapseWhitespace: true})))
+		.pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('build-jekyll', ['build-jekyll-html'], function() {
-  // Clean up after the jekyll build
-  return gulp.src([paths.dest_jekyll], { read: false })
-    .pipe(rimraf());
+	// Clean up after the jekyll build
+	return gulp.src([paths.dest_jekyll], { read: false })
+		.pipe(rimraf());
 });
 
 
@@ -91,8 +92,8 @@ gulp.task('build-jekyll', ['build-jekyll-html'], function() {
 
 // Copy all static files from source/ to publish/
 gulp.task('build-copystatic', ['build-jekyll'], function() {
-  return gulp.src([paths.source + "**", paths.ignore], { base: paths.source })
-    .pipe(gulp.dest(paths.dest));
+	return gulp.src([paths.source + "**", paths.ignore], { base: paths.source })
+		.pipe(gulp.dest(paths.dest));
 });
 
 
@@ -100,52 +101,64 @@ gulp.task('build-copystatic', ['build-jekyll'], function() {
 
 // Perform all transforms.lessc
 gulp.task('build-lessc', ['build-copystatic', 'build-jekyll'], function() {
-  if (!transforms.lessc) throw new Error("transforms.lessc not defined");
-  return gulp.src(transforms.lessc.from, { cwd: paths.source })
-    .pipe(less())
-    .pipe(uncss({
-            html: glob.sync(paths.dest + "**/*.html"),
-            ignore: [/\.scrollspy\-(.*)/, ".sidebar-nav.fixed"]
-          }))
-    .pipe(minifyCSS({noRebase:true, keepSpecialComments:0}))
-    .pipe(gulp.dest(paths.dest + transforms.lessc.to));
+	if (!transforms.lessc) throw new Error("transforms.lessc not defined");
+	return gulp.src(transforms.lessc.from, { cwd: paths.source })
+		.pipe(less())
+		.pipe(uncss({
+						html: glob.sync(paths.dest + "**/*.html"),
+						ignore: [/\.scrollspy\-(.*)/, ".sidebar-nav.fixed"]
+					}))
+		.pipe(minifyCSS({noRebase:true, keepSpecialComments:0}))
+		.pipe(gulp.dest(paths.dest + transforms.lessc.to));
 });
 
 gulp.task('build-jsmin', ['build-copystatic'], function() {
-  if (!transforms.js) throw new Error("transforms.js not defined");
-  return gulp.src(transforms.js.from, { cwd: paths.source })
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.dest + transforms.js.to));
+	if (!transforms.js) throw new Error("transforms.js not defined");
+	return gulp.src(transforms.js.from, { cwd: paths.source })
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.dest + transforms.js.to));
 });
 
+
+// The transformations applied on images to convert them from the source to the export formats.
+// 
+var blogImageTransform = function (im, style) {
+	return lazypipe()
+		.pipe(rename, function (path) {
+				path.dirname = path.dirname.replace(im.parent + im.folder_prefix, im.new_name);
+				path.extname = "." + style.name + path.extname.toLowerCase();
+			})
+		.pipe(imageResize, {
+				width: style.max_width,
+				height: style.max_height,
+				imageMagick: true
+			})
+		// Workaround to prevent optipng bug in imagemin
+		// https://github.com/google/web-starter-kit/issues/279
+		.pipe(gulpif, /.*\.png/, gutil.noop(), imagemin({
+				progressive: true,
+				svgoPlugins: [{removeViewBox: false}],
+				use: [pngcrush()]
+			}));
+};
+	
 // Copy all blog images, scaling and optimizing as necessary.
 //styles: [{name: "thumb", max_width: 600, max_height: 400},
 gulp.task('build-blogimages', ['build-copystatic'], function() {
-  var im = transforms.imagemin;
-  // Parallel from this
-  // http://www.jamescrowley.co.uk/2014/02/17/using-gulp-packaging-files-by-folder/
-  var tasks = im.styles.map(function(style) {
-      return gulp.src(paths.content + "**/" + im.parent + im.folder_prefix + "*/*.*", { base: paths.content })
-        .pipe(rename(function (path) {
-            path.dirname = path.dirname.replace(im.parent + im.folder_prefix, im.new_name);
-            path.extname = "." + style.name + path.extname.toLowerCase();
-          }))
-        .pipe(imageResize({
-            width: style.max_width,
-            height: style.max_height,
-            imageMagick: true
-          }))
-        // Workaround to prevent optipng bug in imagemin
-        // https://github.com/google/web-starter-kit/issues/279
-        .pipe(gulpif(/.*\.png/, gutil.noop(), imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngcrush()]
-          })))
-        .pipe(gulp.dest(paths.dest));
-    });
+	var im = transforms.imagemin;
+	var styleNames = im.styles.map(function (e) { return e.name });
+	// Parallel from this
+	// http://www.jamescrowley.co.uk/2014/02/17/using-gulp-packaging-files-by-folder/
+	var tasks = im.styles.map(function(style) {
+			return gulp.src(paths.content + "**/" + im.parent + im.folder_prefix + "*/*.*", { base: paths.content })
+				.pipe(gulpif(new RegExp(".*\.(" + styleNames.join("|") + ")\..+"),
+						gutil.noop(),
+						blogImageTransform(im, style)()
+					))
+				.pipe(gulp.dest(paths.dest));
+		});
 
-  return es.concat.apply(null, tasks);
+	return es.concat.apply(null, tasks);
 });
 
 
